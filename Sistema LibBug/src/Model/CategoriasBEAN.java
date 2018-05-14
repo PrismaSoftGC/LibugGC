422de850c436a79517feecb6e97134a4198e04f9
Erick Moraes <erickmoraes_rv@hotmail.com>
2018-05-14 00:31:29 -0300

    Att base de dados

---
+++ Sistema LibBug/src/Model/CategoriasBEAN.java
@@ -0,0 +1,34 @@
+
+package Model;
+
+public class CategoriasBEAN {
+    
+    private int codigoCategoria;
+    private String nome;
+
+    public CategoriasBEAN(int codigoCategoria, String nome) {
+        this.codigoCategoria = codigoCategoria;
+        this.nome = nome;
+    }
+    
+    public CategoriasBEAN(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
---
+++ Sistema LibBug/src/Model/CidadesBEAN.java
@@ -0,0 +1,45 @@
+
+package Model;
+
+public class CidadesBEAN {
+    
+    private int codigoCidade;
+    private String nome;
+    private int estado_codigoEstado;
+
+    public CidadesBEAN(int codigoCidade, String nome, int estado_codigoEstado) {
+        this.codigoCidade = codigoCidade;
+        this.nome = nome;
+        this.estado_codigoEstado = estado_codigoEstado;
+    }
+
+    public CidadesBEAN(String nome, int estado_codigoEstado) {
+        this.nome = nome;
+        this.estado_codigoEstado = estado_codigoEstado;
+    }
+
+    public int getCodigoCidade() {
+        return codigoCidade;
+    }
+
+    public void setCodigoCidade(int codigoCidade) {
+        this.codigoCidade = codigoCidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getEstado_codigoEstado() {
+        return estado_codigoEstado;
+    }
+
+    public void setEstado_codigoEstado(int estado_codigoEstado) {
+        this.estado_codigoEstado = estado_codigoEstado;
+    }
+    
+}
