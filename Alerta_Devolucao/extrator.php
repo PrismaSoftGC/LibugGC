7fa8b8983bf6e6ea9cabdd142efc7c7309817f07
lbmendonca1997 <lucasbarroso99@live.com>
2018-06-19 23:52:43 -0300

    Extrator: faltando conexao com o BD

--- Alerta_Devolucao/extrator.php
+++ Alerta_Devolucao/extrator.php
@@ -1,10 +1,32 @@
 <?php
-$para = "to:lucasbarrosomendonca@gmail.com";
-$assunto = "Teste de envio de email";
-$mensagem = "Isto é um teste de envio de email através do PHP";
+
+/*
+Gabriel/Walter
+Preciso que monte a conexao deste select passando o valor para essas variaveis
+Select: SELECT nome, email, devolucao FROM cliente INNER join emprestimo on cliente.codigoCliente = emprestimo.codigoCliente
+
+Ai voce joga em um vetor, que compara a data atual e ve se ela é um dia antes da data de devolucao, se ela for, passa os valores para as variaves e inicia
+a funcao mail, caso nao for... ai voces coloca para dar stop na execucao do php.
+
+Se nao entender chama no zap apos as 04:00 da manha...
+*/
+
+
+
+
+/*
+//variaveis locais recebendo os valores do banco 
+$email = $email;
+$nome = $nome;
+$devolucao = $devolucao;
+*/
+
+$para = "to:$email";
+$assunto = "PrismaSoft - Alerta de Devolução";
+$mensagem = "Olá, $nome voce tem devolução que vence amanha($dataDevolucao), evite pagar multas e faça a devolução.";
 $cabecalho = "MIME-Version: 1.0" . "\r\n";
 $cabecalho .= "Content-type: text/html; charset=ISO 8859-1" . "\r\n";
-$cabecalho .= "from: prismasoftgc<prismasoftgc@gmail.com>" . "\r\n" . "Reply-to: lucasbarrosomendonca@gmail.com" . "\r\n";
+$cabecalho .= "from: prismasoftgc<prismasoftgc@gmail.com>" . "\r\n" . "Reply-to: prismasoftgc@gmail.com" . "\r\n";
 
 mail($para, $assunto, $mensagem, $cabecalho);
 echo "Email enviado com sucesso.";
